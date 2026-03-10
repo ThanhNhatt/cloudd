94c0995de2d0cb962fc9eafebf99c1dbd55521ea
your name <thanhnhathc2004@gnmail.com>
2026-03-10 22:13:06 +0700

    new commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://localhost:5000"
+  baseURL: "https://api-gateway.onrender.com"
 });
 
-API.interceptors.request.use((req)=>{
+API.interceptors.request.use((req) => {
 
- const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
- if(token){
-  req.headers.Authorization = `Bearer ${token}`;
- }
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
 
- return req;
+  return req;
 
 });
 
