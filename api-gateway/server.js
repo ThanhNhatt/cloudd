53267f7b822f9cafacfe55b0d7ae7676d1197375
your name <thanhnhathc2004@gnmail.com>
2026-03-10 22:07:14 +0700

    new commit

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -68,6 +68,8 @@ app.use(
   })
 );
 
-app.listen(5000, () => {
-  console.log("API Gateway running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("API Gateway running on port", PORT);
 });
